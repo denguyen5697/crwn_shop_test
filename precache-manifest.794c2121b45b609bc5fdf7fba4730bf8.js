81d5f012a4f47bc6c151871c851900f4367f1e5e
unknown <ntdvt197@gmail.com>
2020-08-12 02:41:13 +0700

    Updates

---
+++ precache-manifest.794c2121b45b609bc5fdf7fba4730bf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7466590c2c4c2f8c73c09f2c19a327d",
+    "url": "/crwn_shop_test/index.html"
+  },
+  {
+    "revision": "e279f970f8c92e5c54a0",
+    "url": "/crwn_shop_test/static/css/main.5b528f1b.chunk.css"
+  },
+  {
+    "revision": "192d3d01c9b17460e774",
+    "url": "/crwn_shop_test/static/js/2.e364114e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn_shop_test/static/js/2.e364114e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e279f970f8c92e5c54a0",
+    "url": "/crwn_shop_test/static/js/main.5810ed38.chunk.js"
+  },
+  {
+    "revision": "8b919229bd360eafbebd",
+    "url": "/crwn_shop_test/static/js/runtime-main.2536d5df.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/crwn_shop_test/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn_shop_test/static/media/shopping-bag.30f07071.svg"
+  }
+]);
